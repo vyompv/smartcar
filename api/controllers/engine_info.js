e4b0ba5aa78f2e9a3d1acd4a76f05cc85c6cd01b
vyompvscu <vpatikkalveettil@scu.edu>
2016-08-03 19:27:53 -0700

    feat(gm-api): updated multiple files for indentation and spacing

--- api/controllers/engine_info.js
+++ api/controllers/engine_info.js
@@ -10,17 +10,17 @@ function engineInfo(req, res) {
   var id = req.swagger.params.id.value;
   var action = req.swagger.params.action.value;
   var cmd=null;
-  if(action.toLowerCase()=="start")
-  		cmd="START_VEHICLE";
-  else if(action.toLowerCase()=="stop")	
-  		cmd="STOP_VEHICLE";
-  else 	res.json({"status":result.status,"error":"Please send the START or STOP command in the action field"});
+  if(action.toLowerCase() == "start")
+  		cmd = "START_VEHICLE";
+  else if(action.toLowerCase() == "stop")	
+  		cmd = "STOP_VEHICLE";
+  else 	res.json({"status": 500,"error":"Please send the START or STOP command in the action field"});
 	var options = { method: 'POST',
 	  url: 'http://gmapi.azurewebsites.net/actionEngineService',
 	  headers: { 'content-type': 'application/json' },
-	  body: { id: id, command:cmd, responseType: 'JSON' },
+	  body: { id: id, command: cmd, responseType: 'JSON' },
 	  json: true };
-	var result=null;
+	var result = null;
 	var promise = new Promise(function(resolve, reject) {
 		request(options, function (error, response, body) {
 		  if (error) throw new Error(error);
@@ -33,7 +33,7 @@ function engineInfo(req, res) {
 			});
 	});
 	promise.then(function(result) {
-			if(result.status!=200)
+			if(result.status != 200)
 				res.json({"status":result.status,"error":result.reason});
 			var gmResponse = result.actionResult;
 			var smartCarData={};
